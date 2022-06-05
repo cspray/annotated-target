b4e753d15789508bcb381f63ea811b01c4393606
Charles Sprayberry <me@cspray.io>
2022-06-05 13:06:54 -0400

    Throw exception when dir empty

--- src/AnnotatedTargetParserOptionsBuilder.php
+++ src/AnnotatedTargetParserOptionsBuilder.php
@@ -2,12 +2,14 @@
 
 namespace Cspray\AnnotatedTarget;
 
+use Cspray\AnnotatedTarget\Exception\InvalidArgumentException;
+
 final class AnnotatedTargetParserOptionsBuilder {
 
     private function __construct() {}
 
     public static function scanDirectories(string... $dirs) : self {
-
+        throw new InvalidArgumentException('The directories to scan must not include an empty value.');
     }
 
     public function withAttributeTypes() : self {
